f593f1823e703cf78d8755f1a35a319108a3fe60
Hemavathi961989 <vhemavathi007@gmail.com>
2025-10-15 23:08:10 +0100

    Add files via upload
    Completed Week 1 - day 1 assignment

---
+++ DataTypes/primitiveDataTypes.js
@@ -0,0 +1,12 @@
+let userName = "Hemavathi"
+let organization = "TestLeaf"
+let contactNumber = 9945678910
+let knowsAutomation = true
+let usesPlaywright
+
+
+console.log(`The type of userName variable is  ${typeof userName}`)
+console.log(`The type of organization variable is ${typeof organization}`)
+console.log("The type of contactNumber variable is " + typeof contactNumber)
+console.log("The type of knowsAutomation variable is " + typeof knowsAutomation)
+console.log(`The type of usesPlaywright variable is ${typeof usesPlaywright}`)
