ba58d4650f0ac2579777c22510053280d87b33f4
Hemavathi961989 <vhemavathi007@gmail.com>
2025-11-02 07:12:09 +0000

    Add files via upload
    Moved from outside to this folder

---
+++ HomeAssignments/Week2/promise.js
@@ -0,0 +1,29 @@
+function fetchDataFromDatabase() {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            const data = true
+
+            if(data)
+            {
+                console.log("Data fectched successfully!")
+            }
+            else
+            {
+                console.log("Data not found!")
+            }
+        }, 3000)
+    })
+}
+
+    fetchDataFromDatabase()
+    .then((message)=>{
+        console.log(message)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
+
