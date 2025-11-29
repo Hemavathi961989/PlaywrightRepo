9cf6f3022f462678f8bf46e30d95b087707a69c2
Hemavathi961989 <vhemavathi007@gmail.com>
2025-11-29 08:08:05 +0000

    Add only the specific files

---
+++ tests/Classroom/OopsConceepts/browser.ts
@@ -0,0 +1,10 @@
+export class Browser{
+
+    browserType(){
+        console.log("The browser type is Firefox")
+    }
+
+    browserVersion(){
+        console.log("The browser version is 1.0")
+    }
+}
